10ef158e6c1a7c293700a9abcb0ab0e64e3b5728
Sri Surya Kiran <srisuryakiranchanda@gmail.com>
2026-08-11 11:32:29 +0530

    added allowed origins

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 
@@ -10,6 +11,13 @@ from app.modules.payments.router import router as payments_router
 app = FastAPI(
     title=settings.APP_NAME,
     version = settings.APP_VERSION
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
